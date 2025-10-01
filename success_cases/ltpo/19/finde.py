538b7fcc958f42b0b580ed5721aa6c1f8e008e51
kang1717 <46272528+kang1717@users.noreply.github.com>
2025-10-01 21:36:18 +0900

    Delete success_cases directory

--- success_cases/ltpo/19/finde.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-with open('log','r') as f:
-  for fline in f:
-    if fline.split()[4] == '0' and fline.split()[6] == '0':
-      if  float(fline.split()[9]) < -809:     
-        print (fline,end='')
--- success_cases/ltpo/examplenew/find20.py
+++
@@ -1,5 +0,0 @@
-with open('log','r') as f:
-  for fline in f:
-    if fline.split()[3] == '0' and fline.split()[5] == '0':
-#    if fline.split()[3] == '0':
-      print (fline,end='')
